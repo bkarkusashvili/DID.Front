6bc790c479ea56ba42d65b7ef79b38e7b8a38989
bkarkusashvili <beqa.qarqusashvili.20@gmail.com>
2022-06-29 22:46:20 +0400

    add api call

--- src/feature/auth/Auth.jsx
+++ src/feature/auth/Auth.jsx
@@ -1,5 +1,9 @@
 import React, { useMemo } from 'react';
 import { TextField } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import axios from 'axios';
 
 import './Auth.scss';
 
@@ -10,51 +14,58 @@ const form = {
     {
       name: 'email',
       label: 'Email',
-      type: 'email'
+      type: 'email',
+      // validate: yup.email('Enter a valid email').required('Email is required'),
     },
     {
       name: 'password',
       label: 'Password',
-      type: 'password'
+      type: 'password',
     },
   ],
   register: [
     {
       name: 'firstname',
       label: 'First Name',
-      type: 'text'
+      type: 'text',
     },
     {
       name: 'lastname',
       label: 'Last Name',
-      type: 'text'
+      type: 'text',
     },
     {
       name: 'email',
       label: 'Email',
-      type: 'email'
+      type: 'email',
     },
     {
       name: 'password',
       label: 'Password',
-      type: 'password'
+      type: 'password',
     },
     {
       name: 'password_confirmation',
       label: 'Confirm Password',
-      type: 'password'
+      type: 'password',
     },
-  ]
+  ],
 };
 
 export const Auth = ({ type }) => {
   const isLogin = useMemo(() => type === 'login', [type]);
   const isRegister = useMemo(() => type === 'register', [type]);
-
   const title = useMemo(
     () => (isLogin ? 'Log into system' : 'Create your account'),
     [type]
   );
+
+  const { handleChange, handleBlur, handleSubmit } = useFormik({
+    initialValues: {},
+    // validationSchema:
+  });
+
+  const submit = () => {};
 
   return (
     <div id="auth">
@@ -74,6 +85,22 @@ export const Auth = ({ type }) => {
           ))}
         </div>
         <Remember hasReset={isLogin} />
+        <button
+          className={`submit ${type}`}
+          onClick={submit}
+          children={isRegister ? 'Get started' : 'Log in'}
+        />
+        <div className="other-option">
+          {isRegister ? (
+            <>
+              Already have an account? <Link to="/login" children="Log in" />
+            </>
+          ) : (
+            <>
+              Don't have an account? <Link to="/register" children="Register" />
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
