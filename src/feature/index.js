d7e551925a47773bfdab0939dac910dcd3bc81aa
GigaPok <gigapokera@gmail.com>
2022-06-30 23:22:01 +0400

    mainPage

--- src/feature/index.js
+++ src/feature/index.js
@@ -1,3 +1,4 @@
-export * from './dashboard/Dashboard';
-export * from './auth/Auth';
-export * from './auth/components';
+export * from "./dashboard/Dashboard";
+export * from "./auth/Auth";
+export * from "./auth/components";
+export * from "./main/Main";
