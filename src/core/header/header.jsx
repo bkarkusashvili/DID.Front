cb17dd2723db2d9cf79ebea6685dfc5fe217e5f3
GigaPok <gigapokera@gmail.com>
2022-07-01 13:12:53 +0400

    changes

--- src/core/header/header.jsx
+++ src/core/header/header.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './header.scss';
-import logo from '../../assets/images/logo.png';
+import "./header.scss";
+import logo from "../../assets/images/logo.png";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
     <header id="header">
-      <a href="#">
+      <Link href="#">
         <img src={logo} alt="didge logo" />
-      </a>
+      </Link>
       <select>
         <option value="ENG">ENG</option>
         <option value="GE">GE</option>
