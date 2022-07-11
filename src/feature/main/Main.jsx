fc7a44bfa00f8a93f5405d7aa9697ef2dec3f519
GigaPok <gigapokera@gmail.com>
2022-07-11 12:31:06 +0400

    s

--- src/feature/main/Main.jsx
+++ src/feature/main/Main.jsx
@@ -6,23 +6,27 @@ import { Link } from "react-router-dom";
 export const Main = () => {
   return (
     <div id="mainpage">
-      <div className="leftContent">
-        <span>
-          Enhance your online presence
-          <span>Generate quality content</span>
-        </span>
-        <p>
-          Artificial Intelligence that helps you generate and optimize content
-          for social media and websites
-        </p>
-        <Link to="/register">Get started</Link>
+      <div className="section-1">
+        <div className="container">
+          <div className="leftContent">
+            <span>
+              Enhance your online presence
+              <span>Generate quality content</span>
+            </span>
+            <p>
+              Artificial Intelligence that helps you generate and optimize
+              content for social media and websites
+            </p>
+            <Link to="/register">Get started</Link>
+          </div>
+          <div className="list">
+            <span>Social media content</span>
+            <span>Websites</span>
+            <span>Newsletters and Blogs</span>
+          </div>
+        </div>
+        <img src={bgimg}></img>
       </div>
-      <div className="list">
-        <span>Social media content</span>
-        <span>Websites</span>
-        <span>Newsletters and Blogs</span>
-      </div>
-      <img src={bgimg}></img>
     </div>
   );
 };
