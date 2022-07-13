a960e692e56614acc81e11e6568b7b350456c7c1
GigaPok <gigapokera@gmail.com>
2022-07-13 17:04:01 +0400

    main page update

--- src/feature/main/Main.jsx
+++ src/feature/main/Main.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./Main.scss";
 import bgimg from "../../assets/images/mainBG.png";
 import { Link } from "react-router-dom";
+import socialone from "../../assets/images/social-1.png";
+import socialtwo from "../../assets/images/social-2.png";
+import newsite from "../../assets/images/newsite.svg";
+import branding from "../../assets/images/branding.png";
 
 export const Main = () => {
   return (
@@ -26,6 +30,73 @@ export const Main = () => {
           </div>
         </div>
         <img src={bgimg}></img>
+      </div>
+      <div className="section-2">
+        <div className="container">
+          <div className="leftContent">
+            <h2>Social media content</h2>
+            <p>
+              Dogs have proven themselves time and again to be loyal, kind,
+              understanding,
+              <br />
+              <br />
+              may have been the worst day of our lives and make us feel better
+              with a wag of their tail and a playful grin.
+              <br />
+              <br />
+              happily after what may have been the worst day of our lives and
+              make us eir tail and a playful grin.
+            </p>
+          </div>
+          <div className="img-wrapper">
+            <img src={socialone}></img>
+            <img src={socialtwo}></img>
+          </div>
+        </div>
+      </div>
+      <div className="section-3">
+        <div className="container">
+          <div className="leftContent">
+            <h2>Create Website content</h2>
+            <p>
+              Dogs have proven themselves time and again to be loyal, kind,
+              understanding,
+              <br />
+              <br />
+              may have been the worst day of our lives and make us feel better
+              with a wag of their tail and a playful grin.
+              <br />
+              <br />
+              happily after what may have been the worst day of our lives and
+              make us eir tail and a playful grin.
+            </p>
+          </div>
+          <div className="img-wrapper">
+            <img src={newsite}></img>
+          </div>
+        </div>
+      </div>
+      <div className="section-4">
+        <div className="container">
+          <div className="leftContent">
+            <h2>Branding elements applied</h2>
+            <p>
+              Dogs have proven themselves time and again to be loyal, kind,
+              understanding,
+              <br />
+              <br />
+              may have been the worst day of our lives and make us feel better
+              with a wag of their tail and a playful grin.
+              <br />
+              <br />
+              happily after what may have been the worst day of our lives and
+              make us eir tail and a playful grin.
+            </p>
+          </div>
+          <div className="img-wrapper">
+            <img src={branding}></img>
+          </div>
+        </div>
       </div>
     </div>
   );
